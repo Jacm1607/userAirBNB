ccc4071f58a9466f1c25c8088e979181f51fb8c0
Jose Chavarria <jose.chavarria@bg.com.bo>
2023-11-18 02:13:23 -0400

    [FEATURE] users memory

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AppService, User } from './app.service';
 
 @Controller()
 export class AppController {
@@ -9,5 +9,21 @@ export class AppController {
   getHello(@Body() body: any): Promise<any> {
     console.log(body);
     return this.appService.getName(body);
+  }
+
+  @Get('/user/:uuid')
+  findUserByUUID(@Param() uuid: string): Promise<User> {
+    return this.appService.getUserByUUID(uuid);
+  }
+
+  @Get('/users')
+  getUserAll(): void {
+    return this.appService.getUserAll();
+  }
+
+  @Post('/user')
+  createUser(@Body() body: any): Promise<User> {
+    console.log(body);
+    return this.appService.pushUser(body);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,40 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
 
+export type User = any;
 @Injectable()
 export class AppService {
   constructor(@Inject('RABBIT_MQ') private client: ClientProxy) {}
 
+  private readonly users = [
+    {
+      uuid: '903bfe84-efbc-47c9-855f-bbfbf1132306',
+      username: 'john',
+    },
+    {
+      uuid: '12ee0ff3-135c-4556-930d-5db1595c1a80',
+      username: 'maria',
+    },
+  ];
   async getName(name: any): Promise<Observable<any>> {
     return await this.client.emit('ms_reserve', name).toPromise(); //-> 200
     // return await this.client.emit('ms_property', name).toPromise();
+  }
+
+  async getUserByUUID(params: any): Promise<User | undefined> {
+    const user = this.users.find((user) => user.uuid === params.uuid);
+    return user ?? null;
+  }
+
+  getUserAll(): any {
+    return this.users;
+  }
+
+  pushUser(user: User): any {
+    user.uuid = uuidv4();
+    this.users.push(user);
+    return user;
   }
 }
